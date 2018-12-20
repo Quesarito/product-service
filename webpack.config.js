2f54d21fd88c30e5282766a0fec5db3dea3a25b5
Jose Velez <josevelezq7@gmail.com>
2018-12-19 22:47:17 -0800

    Configure webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public'),
+  },
+};
