59d890f54c91facee1e33b88ae450561bb8d24d3
Jose Velez <josevelezq7@gmail.com>
2018-12-19 19:43:32 -0800

    Generate fake data and populate database with 100 products

---
+++ db/fake-data-generator.js
@@ -0,0 +1,66 @@
+// Import fake data npm module
+const faker = require('faker');
+
+// Get a random number of image urls from faker
+const getImgUrls = () => {
+  let numImg = Math.random() * 10 + 1;
+  let images = [];
+
+  for (let i = 0; i < numImg; i += 1) {
+    images.push(faker.image.image());
+  }
+  return images;
+};
+
+// Get a random amount of description from faker
+const getDescription = () => {
+  let rand = Math.floor(Math.random() * 10);
+  let text = [];
+
+  for (let i = 0; i < rand; i += 1) {
+    text.push(faker.lorem.paragraphs() + ' ')
+  }
+  return text.join('');
+};
+
+// Produce a new product document with all fields
+const newProductDocument = (id) => {
+  return {
+    id: id,
+    name: faker.commerce.productName(),
+    description: getDescription(),
+    seller: faker.company.companyName(),
+    prime_eligible: Math.floor(Math.random() * 10) > 4,
+    versions: {
+      new: {
+        qty_in_stock: Math.floor(Math.random() * 25),
+        price: Math.floor(Math.random() * 1000),
+      },
+      old: {
+        qty_in_stock: Math.floor(Math.random() * 25),
+        price: Math.floor(Math.random() * 1000),
+      }
+    },
+    image_urls: getImgUrls(),
+    expected_date_of_arrival: faker.date.future().toString(),
+
+    five_star_reviews: Math.floor(Math.random() * Math.random() * 100),
+    four_star_reviews: Math.floor(Math.random() * Math.random() * 100),
+    three_star_reviews: Math.floor(Math.random() * Math.random() * 100),
+    two_star_reviews: Math.floor(Math.random() * Math.random() * 100),
+    one_star_reviews: Math.floor(Math.random() * Math.random() * 100),
+    total_reviews: Math.floor(Math.random() * 4882),
+
+    answered_questions: Math.floor(Math.random() * 1000)
+  };
+};
+
+// Randomly generated data that will be exported with 100 products
+let data = [];
+for (let i = 0; i < 100; i += 1) {
+  data.push(newProductDocument(i));
+}
+
+module.exports = {
+  data
+};
---
+++ db/index.js
@@ -0,0 +1,60 @@
+// Import mongoose module
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/product_db');
+
+// Import fake data
+const fakeData = require('./fake-data-generator').data;
+debugger;
+
+// Product Schema definition
+const productSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true
+  },
+  name: String,
+  description: String,
+  seller: String,
+  prime_eligible: Boolean,
+  versions: {
+    new: {
+      qty_in_stock: Number,
+      price: Number,
+    },
+    old: {
+      qty_in_stock: Number,
+      price: Number,
+    }
+  },
+  image_urls: [String],
+  expected_date_of_arrival: String,
+
+  // Number of reviews for each star:
+  five_star_reviews: Number,  // Eventually fetched from /reviews API
+  four_star_reviews: Number,
+  three_star_reviews: Number,
+  two_star_reviews: Number,
+  one_star_reviews: Number,
+  total_reviews: Number,
+
+  // Number of answered questions about the product
+  answered_questions: Number // Eventually fetched from /questions API
+});
+
+
+// Products model definition
+const Product = mongoose.model('Product', productSchema);
+
+Product.insertMany(fakeData)
+  .then(() => {
+    'Successfully inserted fake data from database!';
+    debugger;
+  })
+  .catch(() => {
+    console.log('Error inserting fake data from database index.js!!!');
+    debugger;
+  });
+
+module.exports = {
+  Product
+};
