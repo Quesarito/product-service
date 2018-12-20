9cb67c1e1150ce81b77ab8ece75720b2ede14138
Jose Velez <josevelezq7@gmail.com>
2018-12-19 22:48:29 -0800

    Get server running and serving public directory

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-// Server and express setup
+// Express Server and Middleware Imports
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const db = require('../db/index');
+
+
 const app = express();
 const port = 3002;
 
 // Database import
-const db = require('../db/index');
+
+
+app.use(express.static('public'));
+app.use(morgan('tiny'));
+// app.use(bodyParser.json());
+
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
-
