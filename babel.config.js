bbb9e29de1f3172744f2d8276a0fd933635368da
Jose Velez <josevelezq7@gmail.com>
2018-12-26 18:19:46 -0800

    Configure test envir and write initial set of tests for 3 components

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+// Needed for Jest (for now)
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
--- tests/exampleData.js
+++ tests/exampleData.js
@@ -1,41 +1,76 @@
+// const exampleData = {
+//   id: 3,
+//   name: 'Fantastic Metal Cheese',
+//   description: [
+//     'Ea similique cupiditate sed. Tempore explicabo et asperiores qui omnis id rerum. Assumenda qui asperiores dignissimos officia suscipit corporis. Voluptate praesentium ipsam laboriosam qui et qui nisi ut. Culpa quas dignissimos animi et est illum praesentium et qui. Vel dolorem assumenda sed soluta repudiandae. Voluptate hic nonomnis. Voluptates ut atque beatae. Aliquam maiores voluptatem suscipit enim dolores sunt natus quisquam voluptas. Iste nisi inventore autem in illo quaerat dolor ipsam. Rerum eligendi cum et assumenda quo ut eaque dolores. Officia excepturi incidunt qui. Ipsum quo omnis expedita. Ipsa perferendis qui doloribus omnis alias ut qui enim. Quo quo fuga maxime facere sint expedita.',
+//     'Aut suscipit ex. Et unde suscipit ea saepe in a magnam nemo. Aut saepe eum aut similique accusamus nesciunt. Sed deserunt dolorum nihil reiciendis harum ipsa nobismollitia culpa. Numquam est eveniet culpa expedita iusto eum. Est maxime id aut molestias et minus. Dolores maiores ipsam dicta. Ex qui magni voluptas itaque sed nemo enim nulla. Illo perspiciatis voluptatem aperiam praesentium blanditiis cupiditate debitis. Perferendis et alias et et eos et nisi possimus. Est ea rerum ut nostrumquam facilis voluptate iste. Eligendi est in sunt amet eius optio quia. Dignissimos dicta nihil dolor minus et.',
+//     'Rem laboriosam nihil. Labore sit doloremque eos ullam unde sapiente. Quo et iusto impedit asperiores sequi perferendis id accusantium blanditiis. Est ut alias eos omnis vel quos officia expedita iure. Iusto sed aliquid aliquam nesciunt eos aut est. Ipsum asperiores autem. Possimus explicabo ut eaque architecto dolore velit nobis sit. Voluptatem pariatur ea soluta est. Voluptas numquam blanditiis reprehenderit doloribus. Dolorem sit fuga consequuntur a esse excepturi culpa natus amet. Consequatur voluptas reiciendis quae.' ],
+//   seller: 'Crooks, Hettinger and Friesen',
+//   prime_eligible: true,
+//   versions: {
+//     new: {
+//       qty_in_stock: 22,
+//       price: '129.00',
+//     },
+//     old: {
+//       qty_in_stock: 17,
+//       price: '710.00',
+//     },
+//   },
+//   image_urls: [
+//     'https://images-na.ssl-images-amazon.com/images/I/71W3iuXipAL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/91I8OMjMlvL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/81QZFJCd2AL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/81%2BFC80Y0QL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/71tdaVCiaqL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/81uUQbnBKCL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/81q9PvcTWoL._SL1500_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/712-noHsQYL._SL1000_.jpg',
+//     'https://images-na.ssl-images-amazon.com/images/I/A1kjj0Hrv6L._SL1500_.jpg'
+//   ],
+//   expected_date_of_arrival: 'Sat Mar 30 2019 03:48:47 GMT-0700 (PDT)',
+//   five_star_reviews: 16,
+//   four_star_reviews: 25,
+//   three_star_reviews: 76,
+//   two_star_reviews: 1,
+//   one_star_reviews: 9,
+//   total_reviews: 2,
+//   answered_questions: 101,
+// };
+//
+// export default exampleData;
+
 const exampleData = {
-  id: 3,
-  name: 'Fantastic Metal Cheese',
+  id: 0,
+  name: 'Incredible Concrete Towels',
   description: [
-    'Ea similique cupiditate sed. Tempore explicabo et asperiores qui omnis id rerum. Assumenda qui asperiores dignissimos officia suscipit corporis. Voluptate praesentium ipsam laboriosam qui et qui nisi ut. Culpa quas dignissimos animi et est illum praesentium et qui. Vel dolorem assumenda sed soluta repudiandae. Voluptate hic nonomnis. Voluptates ut atque beatae. Aliquam maiores voluptatem suscipit enim dolores sunt natus quisquam voluptas. Iste nisi inventore autem in illo quaerat dolor ipsam. Rerum eligendi cum et assumenda quo ut eaque dolores. Officia excepturi incidunt qui. Ipsum quo omnis expedita. Ipsa perferendis qui doloribus omnis alias ut qui enim. Quo quo fuga maxime facere sint expedita.',
-    'Aut suscipit ex. Et unde suscipit ea saepe in a magnam nemo. Aut saepe eum aut similique accusamus nesciunt. Sed deserunt dolorum nihil reiciendis harum ipsa nobismollitia culpa. Numquam est eveniet culpa expedita iusto eum. Est maxime id aut molestias et minus. Dolores maiores ipsam dicta. Ex qui magni voluptas itaque sed nemo enim nulla. Illo perspiciatis voluptatem aperiam praesentium blanditiis cupiditate debitis. Perferendis et alias et et eos et nisi possimus. Est ea rerum ut nostrumquam facilis voluptate iste. Eligendi est in sunt amet eius optio quia. Dignissimos dicta nihil dolor minus et.',
-    'Rem laboriosam nihil. Labore sit doloremque eos ullam unde sapiente. Quo et iusto impedit asperiores sequi perferendis id accusantium blanditiis. Est ut alias eos omnis vel quos officia expedita iure. Iusto sed aliquid aliquam nesciunt eos aut est. Ipsum asperiores autem. Possimus explicabo ut eaque architecto dolore velit nobis sit. Voluptatem pariatur ea soluta est. Voluptas numquam blanditiis reprehenderit doloribus. Dolorem sit fuga consequuntur a esse excepturi culpa natus amet. Consequatur voluptas reiciendis quae.' ],
-  seller: 'Crooks, Hettinger and Friesen',
-  prime_eligible: true,
+    'Illo veritatis quia nostrum aliquid possimus soluta fugit aut tempore. Perferendis quo est in aperiam qui hic. Cupiditate alias nihil atque.\n \rDolorem non est quibusdam eligendi provident quia. Sunt rerum consequatur et. Consequuntur rem assumenda cupiditate aperiam.\n \rTenetur ut praesentium occaecati quasi molestiae sunt. Quo in praesentium dolores maiores animi maiores expedita. Nihil aliquid deleniti ad sed alias saepe.',
+    'Officia impedit vel at ex ut optio sapiente labore. Praesentium et et fuga asperiores est laborum corporis et facere. Ut in tempore iure pariatur a est. Dignissimos et quos minima expedita vero animi est ut. Similique non eum sint voluptatibus inventore consequuntur similique aperiam.\n \rOmnis ea quo. Facere ex laborum tempora commodi debitis vero quia cumque. Totam et error. Hic facilis ut.\n \rSit dolor eum suscipit. Voluptatem voluptates vero deleniti. Rerum aut autem tempore dolorem.' ],
+  seller: 'Paucek, Bashirian and Veum',
+  prime_eligible: false,
   versions: {
     new: {
-      qty_in_stock: 22,
-      price: '129.00',
+      qty_in_stock: 4,
+      price: '711.00',
     },
     old: {
-      qty_in_stock: 17,
-      price: '710.00',
+      qty_in_stock: 11,
+      price: '251.00',
     },
   },
   image_urls: [
-    'https://images-na.ssl-images-amazon.com/images/I/71W3iuXipAL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/91I8OMjMlvL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/81QZFJCd2AL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/81%2BFC80Y0QL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/71tdaVCiaqL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/81uUQbnBKCL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/81q9PvcTWoL._SL1500_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/712-noHsQYL._SL1000_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/I/A1kjj0Hrv6L._SL1500_.jpg'
+    'https://i.ebayimg.com/images/g/6gQAAOSwW0Vb0KBp/s-l300.jpg',
+    'https://i.ebayimg.com/images/g/hFwAAOSwJ6Nb0KDh/s-l300.jpg',
+    'https://i.ebayimg.com/images/g/U0EAAOSwiiVb0KB5/s-l300.jpg',
   ],
-  expected_date_of_arrival: 'Sat Mar 30 2019 03:48:47 GMT-0700 (PDT)',
-  five_star_reviews: 16,
-  four_star_reviews: 25,
-  three_star_reviews: 76,
-  two_star_reviews: 1,
-  one_star_reviews: 9,
-  total_reviews: 2,
-  answered_questions: 101,
+  expected_date_of_arrival: 'Tue Apr 23 2019 17:27:16 GMT-0700 (PDT)',
+  five_star_reviews: 14,
+  four_star_reviews: 15,
+  three_star_reviews: 8,
+  two_star_reviews: 54,
+  one_star_reviews: 23,
+  total_reviews: 7,
+  answered_questions: 160,
 };
 
 export default exampleData;
