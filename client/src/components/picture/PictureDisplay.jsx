d64892892dc2755edf9fa732f7a76b510bbc0d26
Jose Velez <josevelezq7@gmail.com>
2018-12-27 23:44:19 -0800

    Work on adding tile onMouseOver PictureDisplay image

--- client/src/components/picture/PictureDisplay.jsx
+++ client/src/components/picture/PictureDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Tile from './Tile.jsx';
 
 const Caption = styled.div`
   text-align: center;
@@ -12,20 +13,42 @@ const Caption = styled.div`
 const PrePicture = styled.div`
   img {
     margin-left: 50px; 
-    height: 460px;
-    align-content: center;
     width: auto;
+    max-width: 500px;
+    height: 460px;
+    max-height: 600px;
+    align-content: center;
   }
 `;
 
 class PictureDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.onImageHover = this.onImageHover.bind(this);
+    this.state = {
+      hovered: false,
+      tileCenter_Coords: [],
+    };
+    this.onMouseOverImg = this.onMouseOverImg.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
-  onImageHover() {
-    console.log('hovered');
+  onMouseOverImg(e) {
+    if (this.state.hovered === false) {
+      this.setState({
+        hovered: true,
+        tileCenter_Coords: [e.clientX, e.clientY],
+      });
+    } else {
+      this.setState({
+        tileCenter_Coords: [e.clientX, e.clientY],
+      });
+    }
+  }
+
+  onMouseLeave() {
+    this.setState({
+      hovered: false,
+    });
   }
 
   render() {
@@ -37,10 +60,15 @@ class PictureDisplay extends React.Component {
       margin-top: calc(-1 * ${ this.props.numPictures } * 47.5px);
     `;
 
-    return (
+    return  (
       <Picture>
-        <img onMouseOver={ this.onImageHover } src={ this.props.pictureURL }></img>
-        <Caption>Roll over image to zoom in</Caption>
+        <img onMouseOver={ this.onMouseOverImg } onMouseLeave={ this.onMouseLeave } src={ this.props.pictureURL } ></img>
+        { this.state.hovered ?
+          <Tile xCoord={ this.state.tileCenter_Coords[0] } yCoord={ this.state.tileCenter_Coords[1] }
+            onMouseMoveTile={ this.onMouseMoveTile } onMouseLeave={ this.onMouseLeave } />
+          : ''
+        }
+        <Caption>{ this.state.hovered ? 'Click image to open expanded view' : 'Roll over image to zoom in' }</Caption>
       </Picture>
     );
   }
