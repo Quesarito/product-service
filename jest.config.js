f31491f83a643bc25afe3c120da09b1eb9b339f5
Jose Velez <josevelezq7@gmail.com>
2019-01-01 14:58:02 -0800

    Configure Jest to mock files (.ttf, .jpg, ...); Get code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   clearMocks: true,
+  collectCoverage: false,
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: [
-    '/node_modules/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/tests/',
   ],
+  coverageReporters: [
+    'json',
+    'text',
+    'lcov',
+    'clover',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/tests/__mocks__/styleMock.js',
+  },
   roots: [
     '<rootDir>/tests',
   ],
