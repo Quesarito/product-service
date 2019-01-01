e24209259d1eef75a0f9c5f11615a534329bfb21
Jose Velez <josevelezq7@gmail.com>
2018-12-31 22:18:15 -0800

    Make helpers/ directory for helper functions and content

---
+++ client/src/components/helpers/urls.js
@@ -0,0 +1,9 @@
+const amazonSpriteURL = 'https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIBaseCSS-sprite_2x-13acd72bb22e0c502de00d0afe21c494871da686._V2_.png';
+const primeImgURL = 'https://sguru.org/wp-content/uploads/2018/02/small-and-light-prime-logo._V509606070_.png';
+const tileURL = 'https://images-na.ssl-images-amazon.com/images/G/01/apparel/rcxgs/tile._CB483369105_.gif';
+
+export {
+  amazonSpriteURL,
+  tileURL,
+  primeImgURL,
+};
